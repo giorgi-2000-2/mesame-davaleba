2114b49d10f8d77b0c2236773d43a1be4f82843d
giorgi-2000-2 <gio.mikeladze19990@gmail.com>
2026-01-19 18:47:44 +0400

    mesame davaleba

---
+++ src/test/java/org/example/TestStudent.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import net.datafaker.Faker;
+
+public class TestStudent {
+
+    private String name;
+    private int age;
+    private double grade;
+
+    // Getter & Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Faker faker = new Faker();
+
+        TestStudent student = new TestStudent();
+        student.setName(faker.name().fullName());
+        student.setAge(faker.number().numberBetween(18, 30));
+        student.setGrade(faker.number().randomDouble(2, 60, 100));
+
+        System.out.println("Name: " + student.getName());
+        System.out.println("Age: " + student.getAge());
+        System.out.println("Grade: " + student.getGrade());
+    }
+}
